a1169534946c2b7f8b3ea3e54e50479b7fa6ae59
Jean <jean.derieux@gmail.com>
2021-03-19 02:00:24 +0100

    Add Q6

--- model.js
+++ model.js
@@ -1,3 +1,33 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+
+function Drawing() {
+  this.forms = [];
+};
+
+
+function Form(color, thickness){
+	this.color = color;
+	this.thickness = thickness;
+};
+
+function Rectangle(color, thickness, x_topLeft, y_topLeft, width, height) {
+  Form.call(this, color, thickness)
+
+  this.x_topLeft = x_topLeft;
+  this.y_topLeft = y_topLeft;
+  this.width = width;
+  this.height = height;
+};
+
+
+function Line(color, thickness, x_A, y_A, x_B, y_B) {
+  Form.call(this, color, thickness)
+
+  this.x_A = x_A;
+  this.y_A = y_A;
+  this.x_B = x_B;
+  this.y_B = y_B;
+};
